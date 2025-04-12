8ac6c0bdfb43c609fff37303e30e02090ead37df
Moritz Maier <moritzmaier34@gmail.com>
2025-04-12 20:04:58 +0200

    feat: added more infos

--- src/handling_requests.py
+++ src/handling_requests.py
@@ -16,14 +16,27 @@ def getData(url,address):
             eula: bool = data["eula_blocked"]
             version: str= data["version"]["name_clean"]
             status: bool = data["online"]
-
+            server_icon: str = data["icon"]
+            player_on: int = data["players"]["online"]
+            player_max: int = data["players"]["max"]
+            player_list = []
+            max_player_list = 12
+            
+            if player_on < max_player_list:
+                for index in range(player_on):
+                    try:
+                        player = data["players"]["list"][index]["name_clean"]
+                        player_list.append(player)
+                    except Exception:
+                        player_list = []
+                        print("DEBUG: There was an error while loading the player list")
+    
             print(response.status_code)
         else:
             print(response.status_code)
 
     except Exception as e:
         print(e)
-    
-    return host,port,ip,eula,version,status
+    return host,port,ip,eula,version,status,player_list,player_on,player_max,server_icon
     
 
